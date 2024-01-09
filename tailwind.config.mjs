5079bb386067179df1723cf286294bdc38b9b8cf
daliondev <d4lion.dev@gmail.com>
2024-01-09 18:09:02 -0500

    new(config): add tailwind variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "erie-black": "#1b1d1f",
+        "raisin-black": "#26282B",
+        "outer-space": "#454C53",
+        "slate-gray": "#72787F",
+        "flash-white": "#FBFBFB",
+        "text-gray": "#3F4244",
+      },
+    },
+  },
+  plugins: [],
 }
