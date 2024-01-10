739645312f53b0913a646c17bc072c3f3e3145d4
daliondev <d4lion.dev@gmail.com>
2024-01-10 03:39:28 -0500

    Create home page

---
+++ src/constants/Projects.js
@@ -0,0 +1,28 @@
+const Projects = [
+  {
+    name: "MonoLoad",
+    language: "Python",
+    description:
+      "It is a terminal and gui program focused on downloading videos from the YouTube site.",
+    technologies: ["Terminal", "Python", "Flet", "Gui"],
+    link: "https://github.com/daliondev/monoload",
+  },
+  {
+    name: "Local cloud",
+    language: "JavaScript",
+    description:
+      "This project seeks to facilitate the sending of files on a local network between different devices",
+    technologies: ["Express", "Javascript", "React", "Cloud"],
+    link: "https://github.com/daliondev/local-cloud",
+  },
+  {
+    name: "Huella Verde",
+    language: "Astro.js",
+    description:
+      "Huella Verde is an initiative that seeks to create courses through which young people can learn about the world of agronomy",
+    technologies: ["JavaScript", "Astro.js", "Markdown"],
+    link: "https://github.com/daliondev/huella-verde",
+  },
+]
+
+export default Projects
