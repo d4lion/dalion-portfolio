68e8b0538fee17525f27dd2ec776ba6e7a3bf664
daliondev <d4lion.dev@gmail.com>
2024-01-11 19:14:59 -0500

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,16 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import preact from "@astrojs/preact"
 
+import sitemap from "@astrojs/sitemap"
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact({ compat: true })],
+  site: "https://daliondev.online/",
+  integrations: [
+    tailwind(),
+    preact({
+      compat: true,
+    }),
+    sitemap(),
+  ],
 })
