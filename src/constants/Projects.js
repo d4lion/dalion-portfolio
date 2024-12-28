2b6a71bd1b4238b4b9db330b8dc7efcf05d229fc
d4lion <d4lion.dev@gmail.com>
2024-12-27 22:50:14 -0500

    ✨ update(Projects): add Lovelace Backend and Frontend projects with descriptions and links

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -1,5 +1,21 @@
 const Projects = [
   {
+    name: "Lovelace Backend",
+    language: "Java",
+    description:
+      "Backend project focused on making suggestions and analyzing different data using matrices to indicate possible flight destinations.",
+    technologies: ["JavaEE", "JakartaEE", "PostgreSQL", "Spring Boot"],
+    link: "https://github.com/d4lion/Nodo_Lovelace_Backend_G1"
+  },
+  {
+    name: "Lovelace Frontend",
+    language: "JavaScript",
+    description:
+      "This project was focused on creating an attractive frontend for users of our travel suggestion application",
+    technologies: ["Angular.js", "TailwindCSS", "Rxjs"],
+    link: "https://github.com/d4lion/lovelace-frontend"
+  },
+  {
     name: "MonoLoad",
     language: "Python",
     description:
@@ -14,15 +30,7 @@ const Projects = [
       "This project seeks to facilitate the sending of files on a local network between different devices",
     technologies: ["Express", "Javascript", "React", "Cloud"],
     link: "https://github.com/daliondev/local-cloud",
-  },
-  {
-    name: "Huella Verde",
-    language: "Astro.js",
-    description:
-      "Huella Verde is an initiative that seeks to create courses through which young people can learn about the world of agronomy",
-    technologies: ["Preview", "Astro.js", "Markdown"],
-    link: "https://huellaverde.netlify.app",
-  },
+  }
 ]
 
 export default Projects
