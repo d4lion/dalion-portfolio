814f23f88f27711903fc8148cb99ace6e81f81dd
d4lion <d4lion.dev@gmail.com>
2025-12-15 00:21:13 -0500

    Fix static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import preact from "@astrojs/preact"
-import vercel from "@astrojs/vercel/serverless"
+
 import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://d4lion.site/",
-  output: "server",
-  adapter: vercel(),
+
   integrations: [
     tailwind(),
     preact({
